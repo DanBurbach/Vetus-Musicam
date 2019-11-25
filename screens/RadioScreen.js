9848908415347e686ecdca7670d1262965a94f87
DanBurbach <dburbach1982@gmail.com>
2019-11-25 14:28:25 -0800

    work toward building the class for the Radio play screen

--- screens/RadioScreen.js
+++ screens/RadioScreen.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import {
-    // Image,
-    // Platform,
-    ScrollView,
-    StyleSheet,
-    Text,
-    // TouchableOpacity,
-    // View,
-} from 'react-native';
+import React, { Component } from 'react';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 
-export default function RadioScreen() {
-    return (
-        <ScrollView style= {radiostyles.container}>
-            <Text>Radio here!</Text>
-        </ScrollView>
-    )
+export default class RadioScreen Component {
+    
+    const radiostyles = StyleSheet.create({
+        container: {
+            flex: 1,
+            padding: 15,
+            backgroundColor: '#fff',
+        },
+    });
+
+    RadioScreen.navigationOptions = {
+        title: 'Radio',
+    };
+
+    render() {
+        return (
+            <ScrollView style= {radiostyles.container}>
+                <Text>Radio here!</Text>
+            </ScrollView>
+        )
+    };
 }
 
-RadioScreen.navigationOptions = {
-    title: 'Radio',
-};
 
-const radiostyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 15,
-        backgroundColor: '#fff',
-    },
-});
+
