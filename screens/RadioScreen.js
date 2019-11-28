a4837028954c5c78bd4d65c55e88432be3fbe4d2
DanBurbach <dburbach1982@gmail.com>
2019-11-27 21:34:53 -0800

    update RadioScreen to include needed displayed information about playing music

--- screens/RadioScreen.js
+++ screens/RadioScreen.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import {
-    // Image,
+    Image,
     // Platform,
-    ScrollView,
+    // ScrollView,
     StyleSheet,
     Text,
-    // TouchableOpacity,
-    // View,
+    TouchableOpacity,
+    View,
 } from 'react-native';
+
+import {
+    Ionicons
+} from '@expo/vector-icons';
 
 export default function RadioScreen() {
     return (
-        <ScrollView style= {radiostyles.container}>
-            <Text>Radio here!</Text>
-        </ScrollView>
+        <Text>Radio View</Text>
+        <View style = {styles.container}>
+            <Image style = {styles.albumCover} source = {
+            { uri: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg' }}></Image>
+            <View style = {styles.controls}>
+
+                <TouchableOpacity style = {styles.control} onPress = {() => alert('')}>
+                    <Ionicons name = "ios-skip-backward" size = {48}color = "#444"></Ionicons>
+                </TouchableOpacity> 
+
+                <TouchableOpacity style = {styles.control} onPress = {() => alert('')}>{this.state.isPlaying ? (<Ionicons name = "ios-pause" size = {48} color = "#444"></Ionicons>) : ( <Ionicons name = "ios-play-circle" size = {48} color = "#444"></Ionicons>)} 
+                </TouchableOpacity> 
+
+                <TouchableOpacity style = {styles.control} onPress = {() => alert('')} >
+                    <Ionicons name = "ios-skip-forward" size = {48} color = "#444" />
+                </TouchableOpacity> 
+
+            </View> 
+        </View>
     )
 }
 
@@ -21,10 +41,21 @@ RadioScreen.navigationOptions = {
     title: 'Radio',
 };
 
-const radiostyles = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 15,
         backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-});
+    albumCover: {
+        width: 250,
+        height: 250
+    },
+    controls: {
+        flexDirection: 'row'
+    },
+    control: {
+        margin: 20
+    }
+})
