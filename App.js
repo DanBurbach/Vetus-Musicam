32e7bfe04753bdebf7c624a6aedd4d9e7841bc7c
DanBurbach <dburbach1982@gmail.com>
2019-11-27 21:36:21 -0800

    add package.json updates, .expo needed file, and upate App.js using online tutorial

--- App.js
+++ App.js
@@ -1,62 +1,279 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View, Image, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { Audio } from 'expo-av';
 
-import AppNavigator from './navigation/AppNavigator';
+const audioBookPlaylist = [
+	{
+		title: 'Hamlet - Act I',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri:
+			'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act1_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act II',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri:
+			'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act2_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act III',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri: 'http://www.archive.org/download/hamlet_0911_librivox/hamlet_act3_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act IV',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri:
+			'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act4_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act V',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri:
+			'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act5_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	}
+]
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
+export default class App extends React.Component {
+	state = {
+		isPlaying: false,
+		playbackInstance: null,
+		currentIndex: 0,
+		volume: 1.0,
+		isBuffering: true
+	}
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
-  }
-}
+	async componentDidMount() {
+		try {
+			await Audio.setAudioModeAsync({
+				allowsRecordingIOS: false,
+				interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+				playsInSilentModeIOS: true,
+				interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+				shouldDuckAndroid: true,
+				staysActiveInBackground: true,
+				playThroughEarpieceAndroid: true
+			})
 
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
+			this.loadAudio()
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
+	async loadAudio() {
+		const { currentIndex, isPlaying, volume } = this.state
 
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
+		try {
+			const playbackInstance = new Audio.Sound()
+			const source = {
+				uri: audioBookPlaylist[currentIndex].uri
+			}
+
+			const status = {
+				shouldPlay: isPlaying,
+				volume: volume
+			}
+
+			playbackInstance.setOnPlaybackStatusUpdate(this.onPlaybackStatusUpdate)
+			await playbackInstance.loadAsync(source, status, false)
+			this.setState({
+				playbackInstance
+			})
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	onPlaybackStatusUpdate = status => {
+		this.setState({
+			isBuffering: status.isBuffering
+		})
+	}
+
+	handlePlayPause = async () => {
+		const { isPlaying, playbackInstance } = this.state
+		isPlaying ? await playbackInstance.pauseAsync() : await playbackInstance.playAsync()
+
+		this.setState({
+			isPlaying: !isPlaying
+		})
+	}
+
+	handlePreviousTrack = async () => {
+		let { playbackInstance, currentIndex } = this.state
+		if (playbackInstance) {
+			await playbackInstance.unloadAsync()
+			currentIndex < audioBookPlaylist.length - 1 ? (currentIndex -= 1) : (currentIndex = 0)
+			this.setState({
+				currentIndex
+			})
+			this.loadAudio()
+		}
+	}
+
+	handleNextTrack = async () => {
+		let { playbackInstance, currentIndex } = this.state
+		if (playbackInstance) {
+			await playbackInstance.unloadAsync()
+			currentIndex < audioBookPlaylist.length - 1 ? (currentIndex += 1) : (currentIndex = 0)
+			this.setState({
+				currentIndex
+			})
+			this.loadAudio()
+		}
+	}
+
+	renderFileInfo() {
+		const { playbackInstance, currentIndex } = this.state
+		return playbackInstance ? (
+			<View style={styles.trackInfo}>
+				<Text style={[styles.trackInfoText, styles.largeText]}>
+					{audioBookPlaylist[currentIndex].title}
+				</Text>
+				<Text style={[styles.trackInfoText, styles.smallText]}>
+					{audioBookPlaylist[currentIndex].author}
+				</Text>
+				<Text style={[styles.trackInfoText, styles.smallText]}>
+					{audioBookPlaylist[currentIndex].source}
+				</Text>
+			</View>
+		) : null
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Image
+					style={styles.albumCover}
+					source={{ uri: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg' }}
+				/>
+				<View style={styles.controls}>
+					<TouchableOpacity style={styles.control} onPress={this.handlePreviousTrack}>
+						<Ionicons name='ios-skip-backward' size={48} color='#444' />
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.control} onPress={this.handlePlayPause}>
+						{this.state.isPlaying ? (
+							<Ionicons name='ios-pause' size={48} color='#444' />
+						) : (
+							<Ionicons name='ios-play-circle' size={48} color='#444' />
+						)}
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.control} onPress={this.handleNextTrack}>
+						<Ionicons name='ios-skip-forward' size={48} color='#444' />
+					</TouchableOpacity>
+				</View>
+				{this.renderFileInfo()}
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	albumCover: {
+		width: 250,
+		height: 250
+	},
+	trackInfo: {
+		padding: 40,
+		backgroundColor: '#fff'
+	},
+
+	trackInfoText: {
+		textAlign: 'center',
+		flexWrap: 'wrap',
+		color: '#550088'
+	},
+	largeText: {
+		fontSize: 22
+	},
+	smallText: {
+		fontSize: 16
+	},
+	control: {
+		margin: 20
+	},
+	controls: {
+		flexDirection: 'row'
+	}
+})
+
+// import { AppLoading } from 'expo';
+// import { Asset } from 'expo-asset';
+// import * as Font from 'expo-font';
+// import React, { useState } from 'react';
+// import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+// import { Ionicons } from '@expo/vector-icons';
+
+// import AppNavigator from './navigation/AppNavigator';
+
+// export default function App(props) {
+//   const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+//   if (!isLoadingComplete && !props.skipLoadingScreen) {
+//     return (
+//       <AppLoading
+//         startAsync={loadResourcesAsync}
+//         onError={handleLoadingError}
+//         onFinish={() => handleFinishLoading(setLoadingComplete)}
+//       />
+//     );
+//   } else {
+//     return (
+//       <View style={styles.container}>
+//         {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+//         <AppNavigator />
+//       </View>
+//     );
+//   }
+// }
+
+// async function loadResourcesAsync() {
+//   await Promise.all([
+//     Asset.loadAsync([
+//       require('./assets/images/robot-dev.png'),
+//       require('./assets/images/robot-prod.png'),
+//     ]),
+//     Font.loadAsync({
+//       // This is the font that we are using for our tab bar
+//       ...Ionicons.font,
+//       // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+//       // remove this if you are not using it in your app
+//       'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+//     }),
+//   ]);
+// }
+
+// function handleLoadingError(error) {
+//   // In this case, you might want to report the error to your error reporting
+//   // service, for example Sentry
+//   console.warn(error);
+// }
+
+// function handleFinishLoading(setLoadingComplete) {
+//   setLoadingComplete(true);
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//   },
+// });
