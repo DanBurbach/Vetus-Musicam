4a7371fe23d32fc3a67420b9a1acffe8bf4327c4
DanBurbach <dburbach1982@gmail.com>
2019-11-27 19:27:35 -0800

    rollback of RadioScreen for temporary use

--- screens/RadioScreen.js
+++ screens/RadioScreen.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import React from 'react';
+import {
+    // Image,
+    // Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    // TouchableOpacity,
+    // View,
+} from 'react-native';
 
-export default class RadioScreen Component {
-    
-    const radiostyles = StyleSheet.create({
-        container: {
-            flex: 1,
-            padding: 15,
-            backgroundColor: '#fff',
-        },
-    });
-
-    RadioScreen.navigationOptions = {
-        title: 'Radio',
-    };
-
-    render() {
-        return (
-            <ScrollView style= {radiostyles.container}>
-                <Text>Radio here!</Text>
-            </ScrollView>
-        )
-    };
+export default function RadioScreen() {
+    return (
+        <ScrollView style= {radiostyles.container}>
+            <Text>Radio here!</Text>
+        </ScrollView>
+    )
 }
 
+RadioScreen.navigationOptions = {
+    title: 'Radio',
+};
 
-
+const radiostyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 15,
+        backgroundColor: '#fff',
+    },
+});
