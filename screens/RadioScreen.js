5feef46f9b8e3a08915f53d9b70e8aee963842a0
DanBurbach <dburbach1982@gmail.com>
2020-04-13 22:42:00 -0700

    move older code into PlayerScreen for archiving, adapt styling for darker background, lighter text in styles

--- screens/RadioScreen.js
+++ screens/RadioScreen.js
@@ -1,312 +1,3 @@
-// import React from 'react';
-// import { StyleSheet, TouchableOpacity, View, Image, Text } from 'react-native';
-// import { Ionicons } from '@expo/vector-icons';
-// import { Audio } from 'expo-av';
-// import { audioBookPlaylist } from '../constants/audiobookPlaylist';
-
-// import Slider from 'react-native-slider';
-
-// function getTime(time) {
-// 	if (!isNaN(time)) {
-// 		return (
-// 			Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
-// 		);
-// 	}
-// }
-
-// class RadioScreen extends React.Component {
-// 	constructor(props){
-// 		super(props);
-// 		this.state = {
-// 			isPlaying: false,
-// 			playbackInstance: null,
-// 			currentIndex: 0,
-// 			volume: 1.0,
-// 			isBuffering: true,
-// 			paused: true,
-// 			player: "stopped",
-//       		sliding: false,
-// 			currentTime: null,
-// 			duration: null
-// 		};
-// 		this.onPlaybackStatusUpdate = this.onPlaybackStatusUpdate.bind(this);
-// 	}
-
-// 	async componentDidMount() {
-// 		try {
-// 			await Audio.setAudioModeAsync({
-// 				allowsRecordingIOS: false,
-// 				interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-// 				playsInSilentModeIOS: true,
-// 				interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-// 				shouldDuckAndroid: true,
-// 				staysActiveInBackground: true,
-// 				playThroughEarpieceAndroid: true
-// 			})
-// 			this.loadAudio()
-// 		} catch (e) {
-// 			console.log(e)
-// 		}
-// 		this.player.addEventListener("timeupdate", e => {
-// 			this.setState({
-// 				currentTime: e.target.currentTime,
-// 				duration: e.target.duration
-// 			});
-// 		});
-// 	}
-
-// 	componentWillUnmount() {
-// 	  	this.player.removeEventListener("timeupdate", () => {});
-// 	}
-
-// 	async loadAudio() {
-// 		const { currentIndex, isPlaying, volume } = this.state
-
-// 		try {
-// 			const playbackInstance = new Audio.Sound()
-// 			const source = {
-// 				uri: audioBookPlaylist[currentIndex].uri
-// 			}
-
-// 			const status = {
-// 				shouldPlay: isPlaying,
-// 				volume: volume
-// 			}
-
-// 			playbackInstance.setOnPlaybackStatusUpdate(this.onPlaybackStatusUpdate)
-// 			await playbackInstance.loadAsync(source, status, false)
-// 			this.setState({
-// 				playbackInstance,
-// 			})
-			
-// //display in an alert the track length. To be used in Slider funcitonality
-// 			let playback = playbackInstance.durationMillis
-			
-// 			alert(playback);
-
-// 			// this.setState({
-// 			// 	songLength: this.millisToMinutesAndSeconds(playback.duration.toFixed(0))
-// 			// })
-// 			// alert("song length: " + this.state.songLength)
-
-// 		} catch (e) {
-// 			console.log(e)
-// 		}
-// 	}
-
-// 	millisToMinutesAndSeconds = (millis) => {
-// 		let minutes = Math.floor(millis / 60000);
-// 		let seconds = ((millis % 60000) / 1000).toFixed(0);
-// 		return (seconds == 60 ? (minutes + 1) + ":00" : minutes + ":" + (seconds < 10 ? "0" :
-// 			"") + seconds);
-// 	}
-
-// 	onPlaybackStatusUpdate = status => {
-// 		this.setState({
-// 			isBuffering: status.isBuffering
-// 		})
-// 	}
-
-// 	handlePlayPause = async () => {
-// 		const { isPlaying, playbackInstance } = this.state
-// 		isPlaying ? await playbackInstance.pauseAsync() : await playbackInstance.playAsync()
-
-// 		this.setState({
-// 			isPlaying: !isPlaying
-// 		})
-// 	}
-
-// 	handlePreviousTrack = async () => {
-// 		let { playbackInstance, currentIndex } = this.state
-
-// 		if (playbackInstance) {
-// 			await playbackInstance.unloadAsync()
-// 			currentIndex < audioBookPlaylist.length - 1 ? (currentIndex -= 1) : (currentIndex = 0)
-// 			this.setState({
-// 				currentIndex,
-// 			})
-// 			this.loadAudio()
-// 		}
-// 	}
-
-// 	handleNextTrack = async () => {
-// 		let { playbackInstance, currentIndex } = this.state
-// 		if (playbackInstance) {
-// 			await playbackInstance.unloadAsync()
-// 			currentIndex < audioBookPlaylist.length - 1 ? (currentIndex += 1) : (currentIndex = 0)
-// 			this.setState({
-// 				currentIndex,
-// 			})
-// 			this.loadAudio()
-// 		}
-// 	}
-
-// 	renderFileInfo() {
-// 		const { playbackInstance, currentIndex } = this.state
-// 		return playbackInstance ? (
-// 			<View style={styles.trackInfo}>
-// 				<Text style={[styles.trackInfoText, styles.largeText]}>
-// 					{audioBookPlaylist[currentIndex].title}
-// 				</Text>
-// 				<Text style={[styles.trackInfoText, styles.smallText]}>
-// 					{audioBookPlaylist[currentIndex].author}
-// 				</Text>
-// 				<Text style={[styles.trackInfoText, styles.smallText]}>
-// 					{audioBookPlaylist[currentIndex].source}
-// 				</Text>
-// 			</View>
-// 		) : null
-// 	}
-
-// 	// onLoad() {
-// 	// 	this.setState({
-// 	// 		songDuration: audioBookPlaylist[currentIndex].length
-// 	// 	});
-// 	// 	console.log(this.state.songDuration)
-// 	// }
-
-
-// 	onSlidingStart = async () => {
-// 		this.setState({
-// 			sliding: true
-// 		});
-// 	}
-
-// 	// onSlidingChange = async (value) => {
-// 	// 	let songDuration = this.state.playbackInstance;
-
-
-// 	// 	let newPosition = value * songDuration;
-// 	// 	this.setState({
-// 	// 		currentTime: newPosition
-// 	// 	});
-// 	// }
-
-// 	onSlidingComplete = async () => {
-// 		// this.refs.audio.seek(this.state.currentTime);
-// 		this.setState({
-// 			sliding: false
-// 		});
-// 	}
-
-// 	render() {
-// 		const {currentIndex} = this.state;
-// 		let songPercentage = audioBookPlaylist[currentIndex].length;
-
-// 		return (
-// 			<View style={styles.container}>
-// 				<Image
-// 					style={styles.albumCover}
-// 					source = {
-// 						{
-// 							uri: 'https://images.unsplash.com/photo-1545114687-ab1c9f9fc260?ixlib=rb-1.2.1&auto=format&fit=crop&w=2917&q=80.jpg'
-// 						}
-// 					}
-// 				/>
-// 				<Slider
-// 					onSlidingStart={ this.onSlidingStart.bind(this) }
-// 					onSlidingComplete={ this.onSlidingComplete.bind(this) }
-// 					// onValueChange={ this.onSlidingChange.bind(this) }
-// 					style={ styles.slider }
-// 					trackStyle={ styles.sliderTrack }
-// 					thumbStyle={ styles.sliderThumb }
-// 					maximumValue = {
-// 						songPercentage
-// 					}
-// 					minimumValue={0}
-// 				/>
-// 				<View style={styles.controls}>
-// 					<TouchableOpacity style={styles.control} onPress={this.handlePreviousTrack}>
-// 						<Ionicons name='ios-skip-backward' size={48} color='#444' />
-// 					</TouchableOpacity>
-// 					<TouchableOpacity style={styles.control} onPress={this.handlePlayPause}>
-// 						{this.state.isPlaying ? (
-// 							<Ionicons name='ios-pause' size={48} color='#444' />
-// 						) : (
-// 							<Ionicons name='ios-play-circle' size={48} color='#444' />
-// 						)}
-// 					</TouchableOpacity>
-// 					<TouchableOpacity style={styles.control} onPress={this.handleNextTrack}>
-// 						<Ionicons name='ios-skip-forward' size={48} color='#444' />
-// 					</TouchableOpacity>
-// 				</View>
-// 				{this.renderFileInfo()}
-// 			</View>
-// 		)
-// 	}
-// }
-
-// export default RadioScreen;
-
-// const styles = StyleSheet.create({
-// 	container: {
-// 		flex: 1,
-// 		backgroundColor: '#fff',
-// 		justifyContent: 'center',
-// 		alignItems: 'center'
-// 	},
-// 	albumCover: {
-// 		width: 450,
-// 		height: 450,
-// 		justifyContent: 'center'
-// 	},
-// 	trackInfo: {
-// 		paddingTop: 1,
-// 		backgroundColor: '#fff',
-// 		alignItems: 'center',
-// 		justifyContent: 'center'
-// 		},
-
-// 	trackInfoText: {
-// 		textAlign: 'center',
-// 		flexWrap: 'wrap',
-// 		color: '#550088',
-
-// 	},
-// 	largeText: {
-// 		fontSize: 20
-// 	},
-// 	smallText: {
-// 		fontSize: 16
-// 	},
-// 	control: {
-// 		margin: 25,
-// 		alignItems: 'center',
-// 		justifyContent: 'center'
-// 	},
-// 	controls: {
-// 		flexDirection: 'row',
-// 		alignItems: 'center',
-// 		justifyContent: 'center'
-// 	},
-
-// 	slider: {
-// 		height: 30,
-// 		backgroundColor: '#fff',
-// 		marginTop: 10,
-// 		width: 350
-// 	},
-
-// 	sliderTrack: {
-// 		height: 2,
-// 	},
-
-// 	sliderThumb: {
-// 		width: 10,
-// 		height: 10,
-// 		backgroundColor: '#f62976',
-// 		borderRadius: 10 / 2,
-// 		shadowColor: 'red',
-// 		shadowOffset: {
-// 			width: 0,
-// 			height: 0
-// 		},
-// 		shadowRadius: 2,
-// 		shadowOpacity: 1,
-// 	}
-// })
-
-
 import React, { Component } from 'react';
 import {
 	Dimensions,
@@ -322,18 +13,8 @@ import * as Font from 'expo-font';
 import { MaterialIcons } from '@expo/vector-icons';
 import { audioBookPlaylist } from '../constants/audiobookPlaylist';
 
-// class PlaylistItem {
-// 	constructor(name, uri, image) {
-// 		this.name = name;
-// 		this.uri = uri;
-// 		this.image = image;
-// 	}
-// }
-
-// const PLAYLIST = {audioBookPlaylist};
-
 const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get('window');
-const BACKGROUND_COLOR = '#FFFFFF';
+const BACKGROUND_COLOR = '#000000';
 const DISABLED_OPACITY = 0.5;
 const LOADING_STRING = 'Loading...';
 const BUFFERING_STRING = 'Buffering...';
@@ -348,6 +29,7 @@ class RadioScreen extends Component {
 		this.playbackInstance = null;
 		this.state = {
 			playbackInstanceTitle: LOADING_STRING,
+			playbackInstanceAbout: LOADING_STRING,
 			playbackInstanceAuthor: LOADING_STRING,
 			playbackInstancePosition: null,
 			playbackInstanceDuration: null,
@@ -384,7 +66,6 @@ class RadioScreen extends Component {
 
 	componentWillUnmount() {
 		this.playbackInstance.unloadAsync();
-		//  Check Your Console To verify that the above line is working
 		console.log('unmount');
 	}
 
@@ -417,6 +98,7 @@ class RadioScreen extends Component {
 			this.setState({
 				isPlaying: false,
 				playbackInstanceTitle: LOADING_STRING,
+				playbackInstanceAbout: LOADING_STRING,
 				playbackInstanceAuthor: LOADING_STRING,
 				playbackInstanceDuration: null,
 				playbackInstancePosition: null,
@@ -425,6 +107,7 @@ class RadioScreen extends Component {
 		} else {
 			this.setState({
 				playbackInstanceTitle: audioBookPlaylist[this.index].title,
+				playbackInstanceAbout: audioBookPlaylist[this.index].about,
 				playbackInstanceAuthor: audioBookPlaylist[this.index].author,
 				portrait: audioBookPlaylist[this.index].imageSource,
 				isLoading: false,
@@ -598,6 +281,9 @@ class RadioScreen extends Component {
 						{this.state.playbackInstanceTitle}
 					</Text>
 					<Text style={[styles.text]}>
+						{this.state.playbackInstanceAbout}
+					</Text>
+					<Text style={[styles.text]}>
 						{this.state.playbackInstanceAuthor}
 					</Text>
 					<Text style={[styles.text]}>
@@ -629,7 +315,7 @@ class RadioScreen extends Component {
 							<MaterialIcons
 								name="fast-rewind"
 								size={40}
-								color="#000000"
+								color="#ffffff"
 							/>
 						</View>
 					</TouchableHighlight>
@@ -644,13 +330,13 @@ class RadioScreen extends Component {
 								<MaterialIcons
 									name="pause"
 									size={40}
-									color="#000000"
+									color="#ffffff"
 								/>
 							) : (
 								<MaterialIcons
 									name="play-arrow"
 									size={40}
-									color="#000000"
+									color="#ffffff"
 								/>
 							)}
 						</View>
@@ -665,7 +351,7 @@ class RadioScreen extends Component {
 							<MaterialIcons
 								name="stop"
 								size={40}
-								color="#000000"
+								color="#ffffff"
 							/>
 						</View>
 					</TouchableHighlight>
@@ -679,7 +365,7 @@ class RadioScreen extends Component {
 							<MaterialIcons
 								name="fast-forward"
 								size={40}
-								color="#000000"
+								color="#ffffff"
 							/>
 						</View>
 					</TouchableHighlight>
@@ -716,7 +402,7 @@ class RadioScreen extends Component {
 							<MaterialIcons
 								name="volume-down"
 								size={40}
-								color="#000000"
+								color="#ffffff"
 							/>
 						</View>
 						<Slider
@@ -731,7 +417,7 @@ class RadioScreen extends Component {
 							<MaterialIcons
 								name="volume-up"
 								size={40}
-								color="#000000"
+								color="#ffffff"
 							/>
 						</View>
 					</View>
@@ -746,7 +432,7 @@ class RadioScreen extends Component {
 						<MaterialIcons
 							name="call-received"
 							size={30}
-							color="#000000"
+							color="#ffffff"
 						/>
 					</View>
 					<Slider
@@ -761,7 +447,7 @@ class RadioScreen extends Component {
 						<MaterialIcons
 							name="call-made"
 							size={30}
-							color="#000000"
+							color="#ffffff"
 						/>
 					</View>
 				</View>
@@ -785,11 +471,11 @@ const styles = StyleSheet.create({
 		marginTop: 0,
 	},
 	portrait: {
-		height: 450,
+		height: 300,
 		width: 450,
 	},
 	detailsContainer: {
-		height: 90,
+		height: 150,
 		marginTop: 10,
 		alignItems: 'center',
 	},
@@ -808,9 +494,10 @@ const styles = StyleSheet.create({
 	text: {
 		fontSize: 16,
 		minHeight: 16,
-		marginBottom: 10,
+		marginBottom: 20,
 		justifyContent: 'center',
-		alignItems: 'center'
+		alignItems: 'center',
+		color: '#ffffff'
 	},
 	sliderThumb: {
 		width: 12,
